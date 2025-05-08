b0b581da9de41b5894a4573caa33de271e5a37b6
GOD07thala <sushanthytgaming@gmail.com>
2025-05-08 17:02:25 +0530

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,9 +1,9 @@
 <?php
 
-$token = "YOUR_BOT_TOKEN";
+$token = "7987665609:AAGoXQ4o2dDSRkTH9CKbO_itBR4m9W6hIuU";
 $data = [
     'text' => $msg,
-    'chat_id' => 'YOUR_CHAT_ID'
+    'chat_id' => '6722166633'
 ];
 
 ?>
